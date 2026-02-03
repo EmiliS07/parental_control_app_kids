1a426686faff40f7eafc5df30fea9cbb37acc7f8
EmiliSpot07 <omar.salcedo.lopez02@gmail.com>
2026-02-03 09:06:42 -0600

    Permissions request added

--- app/src/main/java/com/example/parental_control_child/MainActivity.kt
+++ app/src/main/java/com/example/parental_control_child/MainActivity.kt
@@ -1,26 +1,51 @@
 package com.example.parental_control_child
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import android.Manifest
+import android.os.Build
+import android.provider.Settings
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
-    private val locationPermissionCode = 100
+    private val permissionCode = 100
 
-
-    // onCreate: Cuando se crea la actividad (primera vez que se abre)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Concecta con el diseño de la app (layout)
         setContentView(R.layout.activity_main)
 
-        // Pedir los permisos de ubicacion
+        // Permisos basicos
+        val permisos = mutableListOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION,
+            android.Manifest.permission.ACCESS_COARSE_LOCATION
+        )
+
+        // Permisos de notificaciones (Andrdoid +13)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            permisos.add(android.Manifest.permission.POST_NOTIFICATIONS)
+        }
+
+        // Requerir permisos
         ActivityCompat.requestPermissions(
             this,
-            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
-            locationPermissionCode
+            permisos.toTypedArray(),
+            permissionCode
         )
+
+        @SuppressLint("BatteryLife")
+        val intent = Intent(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS)
+        intent.data = "package:$packageName".toUri()
+        startActivity(intent)
+
+        // Requerir ubicacion en segundo plano
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                permissionCode + 1
+            )
+        }
     }
 }
