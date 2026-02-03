533a285bb9fc73cdf4baedbb6e0308363ade71fc
EmiliSpot07 <omar.salcedo.lopez02@gmail.com>
2026-02-03 08:32:08 -0600

    Add location permission

--- app/src/main/java/com/example/parental_control_child/MainActivity.kt
+++ app/src/main/java/com/example/parental_control_child/MainActivity.kt
@@ -2,8 +2,12 @@ package com.example.parental_control_child
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import android.Manifest
 
 class MainActivity : AppCompatActivity() {
+    private val locationPermissionCode = 100
+
 
     // onCreate: Cuando se crea la actividad (primera vez que se abre)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +15,12 @@ class MainActivity : AppCompatActivity() {
 
         // Concecta con el diseño de la app (layout)
         setContentView(R.layout.activity_main)
+
+        // Pedir los permisos de ubicacion
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION),
+            locationPermissionCode
+        )
     }
 }
